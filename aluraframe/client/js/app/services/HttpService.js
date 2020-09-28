26e952fd6e9ec44e7fa09d3260995bdf1ed9886f
johnsjohns <o-anao@hotmail.com>
2020-09-28 13:10:43 -0300

    adicionado metodo post

--- aluraframe/client/js/app/services/HttpService.js
+++ aluraframe/client/js/app/services/HttpService.js
@@ -4,22 +4,41 @@ class HttpService {
         return new Promise((resolve, reject) => {
 
             let xhr = new XMLHttpRequest();
- 
+
             xhr.open('GET', url);
 
             xhr.onreadystatechange = () => {
-                if(xhr.readyState == 4){
-                    if(xhr.status == 200){
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                reject(xhr.responseText);
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.responseText);
+                    }
+
                 }
-                    
-            }
-        };
+            };
 
             xhr.send();
 
+        });
+    }
+
+    post(url, dado) {
+
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest();
+            xhr.open('POST', url, true);
+            xhr.setRequestHeader("Content-Type", "application/json")
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+                        reject(xhr.responseText)
+                    }
+                }
+            };
+            xhr.send(JSON.stringify(dado));
         });
     }
 }

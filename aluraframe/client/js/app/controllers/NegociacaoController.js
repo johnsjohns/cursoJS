12670bf811f829be9f0df1fa736cddf0b021ca9b
johnsjohns <o-anao@hotmail.com>
2020-09-29 13:23:59 -0300

    Finalizando o curso

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -1,66 +1,70 @@
 class NegociacaoController {
-    
+
     constructor() {
-        
+
         let $ = document.querySelector.bind(document);
-        
+
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
-         
+
         this._listaNegociacoes = new Bind(
-            new ListaNegociacoes(), 
-            new NegociacoesView($('#negociacoesView')), 
+            new ListaNegociacoes(),
+            new NegociacoesView($('#negociacoesView')),
             'adiciona', 'esvazia');
-       
+
         this._mensagem = new Bind(
             new Mensagem(), new MensagemView($('#mensagemView')),
-            'texto');       
-    }
-    
-    adiciona(event) {
-        
-        event.preventDefault();
-        this._listaNegociacoes.adiciona(this._criaNegociacao());
-        this._mensagem.texto = 'Negociação adicionada com sucesso'; 
-        this._limpaFormulario();   
+            'texto');
     }
 
-    importaNegociacoes(){
+    adiciona(event) {
+
+        event.preventDefault();
+        try {
+            this._listaNegociacoes.adiciona(this._criaNegociacao());
+            this._mensagem.texto = 'Negociação adicionada com sucesso';
+            this._limpaFormulario();
+        } catch (erro) {
+            this._mensagem.texto = erro;
+        }
+    }
+
+    importaNegociacoes() {
         let service = new NegociacoesService();
-        
+
         Promise.all([
-            service.obterNegociacoesDaSemana(), 
-            service.obterNegociacoesDaSemanaAnterior(), 
-            service.obterNegociacoesDaSemanaRetrasada()
-        ]).then(negociacoes => {
-            negociacoes
-                .reduce((arrayAchatado, array) => arrayAchatado.concat(array),[])
-                .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso';
-        })
-        .catch(error => this._mensagem.texto = error);
+                service.obterNegociacoesDaSemana(),
+                service.obterNegociacoesDaSemanaAnterior(),
+                service.obterNegociacoesDaSemanaRetrasada()
+            ]).then(negociacoes => {
+                negociacoes
+                    .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+                    .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                this._mensagem.texto = 'Negociações importadas com sucesso';
+            })
+            .catch(error => this._mensagem.texto = error);
 
     }
     apaga() {
-        
+
         this._listaNegociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
-    
+
     _criaNegociacao() {
-        
+
         return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value);    
+            this._inputValor.value);
     }
-    
+
     _limpaFormulario() {
-     
+
         this._inputData.value = '';
         this._inputQuantidade.value = 1;
         this._inputValor.value = 0.0;
-        this._inputData.focus();   
+        this._inputData.focus();
     }
 }
--- aluraframe/client/js/app/models/Mensagem.js
+++ aluraframe/client/js/app/models/Mensagem.js
@@ -1,17 +1,17 @@
 class Mensagem {
-    
-    constructor(texto='') {
-        
-        this._texto = texto;
+
+    constructor(texto) {
+
+        this._texto = texto || ''; //  Se texto for undefined, vai passar ''
     }
-    
+
     get texto() {
-        
+
         return this._texto;
     }
-    
+
     set texto(texto) {
-        
+
         this._texto = texto;
     }
 }
